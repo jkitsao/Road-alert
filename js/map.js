15b39c16898ac5f92e0fc3851dec26281c9b149f
Jackson Kitsao <kitsaojackson22@gmail.com>
2020-08-06 18:14:53 +0300

    add ability to change map style

--- js/map.js
+++ js/map.js
@@ -13,3 +13,24 @@ map.addControl(
   }),
   "top-left"
 );
+
+var layerList = document.getElementById("menu");
+var inputs = layerList.getElementsByTagName("input");
+
+function switchLayer(layer) {
+  var layerId = layer.target.id;
+  map.setStyle("mapbox://styles/mapbox/" + layerId);
+}
+
+for (var i = 0; i < inputs.length; i++) {
+  inputs[i].onclick = switchLayer;
+}
+
+map.addControl(
+  new mapboxgl.GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    trackUserLocation: true,
+  })
+);
