46708e3d3eec1cd2f3c6e09f2d91060e336669db
collins koech <collinskoech875@gmail.com>
2020-08-07 08:28:00 +0300

    add user's profile page

--- js/chart.js
+++ js/chart.js
@@ -614,15 +614,4 @@ window.setInterval(function() {
             text: p3Data + "%",
         },
     });
-}, 3000); <
-script >
-    // Get the modal
-    var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    } <
-    /script>
+}, 3000);
